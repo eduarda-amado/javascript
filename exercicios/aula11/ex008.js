ca6105d004797d87cce6301b3ff27554f79316b9
Maria Eduada Amado <slvmadu.contato@gmail.com>
2026-05-06 17:36:11 -0300

    atualizado

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // condção simples!
+    console.log(`Você ultrapassou a veloccidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
