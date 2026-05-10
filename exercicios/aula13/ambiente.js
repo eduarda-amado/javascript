7298665e031c485a5fbcf911734dbc3a037dd6d6
Maria Eduada Amado <slvmadu.contato@gmail.com>
2026-05-10 13:17:06 -0300

    atualizado

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,24 @@
+
+// estrutura de repetição com teste lógico no início
+// primeiro testa, depois executa o bloco
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}*/
+
+// primeiro exetuta o bloco, depois testa
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
